e4f74ca43497a4c4066ef8430c9336df07826a85
Ondrej Certik <ondrej@certik.cz>
2009-10-15 16:55:25 -0700

    Add style.css

--- urls.py
+++ urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls.defaults import *
 
+import os.path
+p = os.path.join(os.path.dirname(__file__), 'media_files/')
+
 urlpatterns = patterns('',
     (r'^$', 'bugzilla_ui.ui.views.redirect_index'),
     (r'^ui/', include('bugzilla_ui.ui.views')),
+    (r'^media_files/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': p}),
 )
