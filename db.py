528e3cb1adbdcac10b7c9051cbb6b285e5b050ea
Ondrej Certik <ondrej@certik.cz>
2009-10-15 13:06:58 -0700

    refactoring

--- db.py
+++ db.py
@@ -1,15 +1,31 @@
 from sqlalchemy import (Table, Column, Integer, String, MetaData, ForeignKey,
         DateTime, create_engine)
-from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy.orm import mapper, sessionmaker, relation
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class Longdescs(Base):
+    __tablename__ = "longdescs"
+
+    bug_id = Column(Integer, ForeignKey("bugs.bug_id"))
+    thetext = Column(String)
+    bug_when = Column(DateTime, primary_key=True)
+
+
+    def __init__(self, bug_id, thetext):
+        self.bug_id = bug_id
+        self.thetext = thetext
+
+    def __repr__(self):
+        return "<Longdescs bug_id=%d>" % (self.bug_id)
 
 class Bugs(Base):
     __tablename__ = "bugs"
 
     bug_id = Column(Integer, primary_key=True)
     product_id = Column(Integer)
+    longdescs = relation(Longdescs, backref="longdescs")
 
     def __init__(self, bug_id, product_id):
         self.bug_id = bug_id
@@ -19,19 +35,6 @@ class Bugs(Base):
         return "<Bugs bug_id=%d, product_id=%d>" % (self.bug_id,
                 self.product_id)
 
-class Longdescs(Base):
-    __tablename__ = "longdescs"
-
-    bug_id = Column(Integer, ForeignKey(Bugs.bug_id))
-    thetext = Column(String)
-    bug_when = Column(DateTime, primary_key=True)
-
-    def __init__(self, bug_id, thetext):
-        self.bug_id = bug_id
-        self.thetext = thetext
-
-    def __repr__(self):
-        return "<Longdescs bug_id=%d>" % (self.bug_id)
 
 
 engine = create_engine("mysql://bugzilla:pass@localhost/bugzilla",
@@ -39,11 +42,12 @@ engine = create_engine("mysql://bugzilla:pass@localhost/bugzilla",
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def show_bug(bug_id):
-    desc = session.query(Longdescs).filter(Longdescs.bug_id==bug_id).all()
+def show_bug(bug):
+    desc = bug.longdescs
+    #desc = session.query(Longdescs).filter(Longdescs.bug_id==bug_id).all()
     print "s"
     print desc
 
 print "bugs 116:"
 bug = session.query(Bugs).filter(Bugs.bug_id==116).one()
-show_bug(bug.bug_id)
+show_bug(bug)
