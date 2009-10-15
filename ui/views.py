365a94e094849a1635fc5979cc72e230491805cb
Ondrej Certik <ondrej@certik.cz>
2009-10-15 16:38:43 -0700

    list all bugs

--- ui/views.py
+++ ui/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render_to_response
 from django.conf.urls.defaults import patterns
 from django.http import HttpResponseRedirect
 
+from models import Bugs
+
 urlpatterns = patterns('bugzilla_ui.ui.views',
     (r'^$',      'index_view'),
 )
 
 def index_view(request):
-    return render_to_response("index.html")
+    bugs = Bugs.objects.all()
+    return render_to_response("index.html", {"bugs": bugs})
 
 def redirect_index(request):
     return HttpResponseRedirect("ui/")
