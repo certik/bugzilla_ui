73b86f36105d6cc24a3237bab4088a3da0d900f9
Ondrej Certik <ondrej@certik.cz>
2009-10-15 16:49:36 -0700

    Print comments

--- ui/views.py
+++ ui/views.py
@@ -5,12 +5,17 @@ from django.http import HttpResponseRedirect
 from models import Bugs
 
 urlpatterns = patterns('bugzilla_ui.ui.views',
-    (r'^$',      'index_view'),
+    (r'^$', 'index_view'),
+    (r'^bug/(\d+)/$', 'bug_view'),
 )
 
 def index_view(request):
-    bugs = Bugs.objects.all()
+    bugs = Bugs.objects.order_by("bug_id").reverse()
     return render_to_response("index.html", {"bugs": bugs})
+
+def bug_view(request, bug_id):
+    bug = Bugs.objects.get(bug_id=bug_id)
+    return render_to_response("bug.html", {"bug": bug})
 
 def redirect_index(request):
     return HttpResponseRedirect("ui/")
