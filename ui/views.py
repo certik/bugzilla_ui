9b375de3106083c55eef8888799acc6395bbccb7
Ondrej Certik <ondrej@certik.cz>
2009-10-15 20:17:00 -0700

    Search box added

--- ui/views.py
+++ ui/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.conf.urls.defaults import patterns
 from django.http import HttpResponseRedirect
+from django import forms
 
 from models import Bugs
 
@@ -9,9 +10,26 @@ urlpatterns = patterns('bugzilla_ui.ui.views',
     (r'^bug/(\d+)/$', 'bug_view'),
 )
 
+class SearchForm(forms.Form):
+    issue_types = forms.ChoiceField([
+        (0, "All issues"),
+        (1, "Open issues"),
+        ], initial=1)
+    search_text = forms.CharField()
+
 def index_view(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            print "form is ok"
+    else:
+        form = SearchForm()
+
     bugs = Bugs.objects.order_by("bug_id").reverse()
-    return render_to_response("index.html", {"bugs": bugs})
+    return render_to_response("index.html", {
+        "bugs": bugs,
+        "form": form,
+        })
 
 def bug_view(request, bug_id):
     bug = Bugs.objects.get(bug_id=bug_id)
